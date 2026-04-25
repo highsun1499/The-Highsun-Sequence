2fa3bb186234c20ca3ef8295e9c0187e0b9f6bd0
highsun1499 <highsun1499@gmail.com>
2026-04-26 02:18:31 +0900

    Update graph.py

--- graph.py
+++ graph.py
@@ -3,12 +3,10 @@ import ipywidgets as widgets
 from IPython.display import display
 import matplotlib.pyplot as plt
 
-# 소수 500개 미리 뽑아놓기
 primes = list(sp.primerange(1, sp.prime(1000) + 1))
 
 def plot_formula(n):
     a_vals =[]
-    # 파이썬의 문자열 합치기 신공(math보다 1000배 빠름)
     for i in range(1, n + 1):
         s = "".join(str(p) for p in reversed(primes[:i]))
         a_vals.append(1.0 / float("0." + s[:17]))
@@ -25,6 +23,5 @@ def plot_formula(n):
     ax.grid(color='gray', alpha=0.3)
     plt.show()
 
-# 슬라이더 만들기
 slider = widgets.IntSlider(value=10, min=1, max=1000, step=1, description='n Value:')
 widgets.interact(plot_formula, n=slider)
